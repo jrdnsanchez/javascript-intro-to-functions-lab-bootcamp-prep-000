6b55ac22178e5a5d5ce89f17d1f414074c9b6001
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-08-30 02:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma (string) {
   var loveAnswer = "I love you, too."
   if (string.toLowerCase(string) === string) return cantAnswer;
 else if (string.toUpperCase(string) === string) return yesAnswer;
-else if ("I love you, Grandma." === string) return loveAnswer;
+else if ("I love you, Gandma." === string) return loveAnswer;
 }
