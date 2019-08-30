acc0a13ce71a4100078362be0af0d7452dd3bb77
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-08-30 02:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ return string.toLowerCase()
 function logShout (string) {
   console.log(string.toUpperCase());
 } 
-function logWhisper (whisper) {
+function logWhisper(string) {
   console.log(string.toLowerCase());
 }
